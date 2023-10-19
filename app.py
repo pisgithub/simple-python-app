b41c567443d05245492d043610cae672af899608
pisgithub <sohail.awan.official@live.com>
2023-10-19 18:24:22 +0500

    back to python project

---
+++ app.py
@@ -0,0 +1,20 @@
+# Python script with intentional errors
+
+# Syntax error: Missing a closing parenthesis
+print("Hello, World"
+
+# Name error: Using a variable before defining it
+age = 25
+print(name)
+
+# Indentation error: Inconsistent indentation
+if True:
+print("This is indented incorrectly")
+
+# ZeroDivisionError: Dividing by zero
+result = 10 / 0
+
+# Type error: Mixing data types
+num1 = "10"
+num2 = 5
+result = num1 + num2
